f843df3cefafe84a63e5435578208b1090765ab9
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2021-08-27 21:52:01 +0300

    Add runtime unit-tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,25 @@
-#include "lexer.h"
+#include "runtime.h"
 #include "test_runner_p.h"
 
-#include <iostream>
+using namespace std;
 
-namespace parse {
-void RunOpenLexerTests(TestRunner& tr);
+namespace runtime {
+void RunObjectHolderTests(TestRunner& tr);
+void RunObjectsTests(TestRunner& tr);
+}  // namespace runtime
+
+namespace {
+
+void TestAll() {
+    TestRunner tr;
+    runtime::RunObjectHolderTests(tr);
+    runtime::RunObjectsTests(tr);
 }
 
+}  // namespace
+
 int main() {
-    try {
-        TestRunner tr;
-        parse::RunOpenLexerTests(tr);
-        parse::Lexer lexer(std::cin);
-        parse::Token t;
-        while ((t = lexer.CurrentToken()) != parse::token_type::Eof{}) {
-            std::cout << t << std::endl;
-            lexer.NextToken();
-        }
-    } catch (const std::exception& e) {
-        std::cerr << e.what();
-        return 1;
-    }
+    TestAll();
+
+    return 0;
 }
